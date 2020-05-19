4aabf8b7b6ec6082551927082f289ebb5644075d
gkampitakis <gkabitakis@gmail.com>
2020-05-19 19:28:26 +0100

    initial commit

--- nodeAuth/src/router/index.js
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import UserRouter from '../modules/user/routes';
-import LocalAuthRouter from '../security/index';
-
-const router = Router();
-
-router.use('/users', UserRouter);
-router.use('/auth', LocalAuthRouter);
-export default router;
