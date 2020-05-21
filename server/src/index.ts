54f7f2705e0ec92fa9dfa1cacd787d77166ee3de
gkampitakis <gkabitakis@gmail.com>
2020-05-21 20:48:04 +0100

    add some dev tools

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,25 +1,21 @@
-import fastify, { FastifyInstance } from 'fastify';
-import cors from 'fastify-cors';
+import fastify, { FastifyInstance } from "fastify";
+import cors from "fastify-cors";
 
 class Server {
+  private server: FastifyInstance;
 
-    private server: FastifyInstance;
+  constructor() {
+    this.server = fastify();
+    this.setup();
+  }
 
-    constructor() {
-        this.server = fastify();
-        this.setup();
-    }
+  private setup(): void {
+    this.server.register(cors, { origin: "*" });
+  }
 
-    private setup(): void {
-
-        this.server.register(cors, { origin: '*' });
-
-    }
-
-    public start(port: number) {
-        return this.server.listen(port, '0.0.0.0');
-    }
-
+  public start(port: number) {
+    return this.server.listen(port, "0.0.0.0");
+  }
 }
 
 export default new Server();
