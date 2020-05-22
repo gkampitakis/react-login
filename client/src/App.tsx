5b6e33912f0909dda9cdac654530352238553f43
gkampitakis <gkabitakis@gmail.com>
2020-05-22 23:24:13 +0100

    [client] Start creating basic setup on client

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,24 +1,17 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import axios from "axios";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <a
+        className="button github-button"
+        href={`https://github.com/login/oauth/authorize?client_id=${"163f2f2581d8325b42b4"}&scope=repo`}
+      >
+        Test login
+      </a>
     </div>
   );
 }
---
+++ client/src/configuration.ts
@@ -0,0 +1,4 @@
+export default {
+  api: process.env.REACT_APP_SERVER_URL,
+  gh_client_id: process.env.REACT_APP_GH_CLIENT_ID,
+};
