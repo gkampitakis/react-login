af36254470af2828d7ae7129cba2d68335370d42
gkampitakis <gkabitakis@gmail.com>
2020-05-24 17:50:56 +0100

    [client] update github token scope and configuration

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,12 @@
 import React from "react";
-import logo from "./logo.svg";
-import axios from "axios";
+import configuration from "./configuration";
 
 function App() {
   return (
     <div className="App">
       <a
         className="button github-button"
-        href={`https://github.com/login/oauth/authorize?client_id=${"163f2f2581d8325b42b4"}&scope=repo`}
+        href={`https://github.com/login/oauth/authorize?client_id=${configuration.gh.clientId}&scope=user:read,user:email`}
       >
         Test Github Login
       </a>
--- client/src/configuration.ts
+++ client/src/configuration.ts
@@ -1,4 +1,7 @@
 export default {
   api: process.env.REACT_APP_SERVER_URL,
-  gh_client_id: process.env.REACT_APP_GH_CLIENT_ID,
+  gh: {
+    clientId: process.env.REACT_APP_GH_CLIENT_ID,
+  },
+  twitter: {},
 };
