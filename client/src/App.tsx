6e0ba87feaa4a31c46cb46463e5782b078bf9cd3
gkampitakis <gkabitakis@gmail.com>
2020-05-25 13:01:07 +0100

    [client] setup twitter authentication

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,11 +3,13 @@ import configuration from "./configuration";
 import { stringify } from "query-string";
 
 function App() {
-  const fbParams = useMemo(() => {
+  const { twitter, fb, gh } = configuration;
+
+  const fb_params = useMemo(() => {
     return stringify({
       client_id: configuration.fb.clientId,
-      redirect_uri: "http://localhost/api/auth/fb/callback",
-      scope: ["email"].join(","),
+      redirect_uri: fb.callback,
+      scope: fb.scope,
       response_type: "code",
       auth_type: "rerequest",
     });
@@ -15,14 +17,9 @@ function App() {
 
   return (
     <div className="App">
-      <a
-        href={`https://github.com/login/oauth/authorize?client_id=${configuration.gh.clientId}&scope=user:read,user:email`}
-      >
-        Test Github Login
-      </a>
-      <a href={`https://www.facebook.com/v4.0/dialog/oauth?${fbParams}`}>
-        Test Facebook Login
-      </a>
+      <a href={gh.url()}>Test Github Login</a>
+      <a href={fb.url(fb_params)}>Test Facebook Login</a>
+      <a href={twitter.url}>Test Twitter Login</a>
     </div>
   );
 }
--- client/src/configuration.ts
+++ client/src/configuration.ts
@@ -2,9 +2,18 @@ export default {
   api: process.env.REACT_APP_SERVER_URL,
   gh: {
     clientId: process.env.REACT_APP_GH_CLIENT_ID,
+    url: function () {
+      return `https://github.com/login/oauth/authorize?client_id=${this.clientId}&scope=user:read,user:email`;
+    },
   },
-  twitter: {},
+  twitter: {
+    url: "http://localhost/api/auth/twitter/request-token",
+  },
   fb: {
     clientId: process.env.REACT_APP_FB_CLIENT_ID,
+    url: (params: string) =>
+      `https://www.facebook.com/v4.0/dialog/oauth?${params}`,
+    callback: "http://localhost/api/auth/fb/callback",
+    scope: ["email"].join(","),
   },
 };
