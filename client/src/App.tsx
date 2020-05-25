734a9283e91e2f9d72621fddae219aa8ca26bea0
gkampitakis <gkabitakis@gmail.com>
2020-05-25 19:19:58 +0100

    [client] setup google authentication

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,15 +3,26 @@ import configuration from "./configuration";
 import { stringify } from "query-string";
 
 function App() {
-  const { twitter, fb, gh } = configuration;
+  const { twitter, fb, gh, google } = configuration;
 
   const fb_params = useMemo(() => {
     return stringify({
-      client_id: configuration.fb.clientId,
+      client_id: configuration.fb.client_id,
       redirect_uri: fb.callback,
       scope: fb.scope,
       response_type: "code",
       auth_type: "rerequest",
+    });
+  }, []);
+
+  const google_params = useMemo(() => {
+    return stringify({
+      client_id: configuration.google.client_id,
+      redirect_uri: google.callback,
+      scope: google.scope,
+      response_type: "code",
+      access_type: "offline",
+      prompt: "consent",
     });
   }, []);
 
@@ -20,7 +31,7 @@ function App() {
       <a href={gh.url()}>Test Github Login</a>
       <a href={fb.url(fb_params)}>Test Facebook Login</a>
       <a href={twitter.url}>Test Twitter Login</a>
-      <a href={""}>Test Google Login</a>
+      <a href={google.url(google_params)}>Test Google Login</a>
     </div>
   );
 }
