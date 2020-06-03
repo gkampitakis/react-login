84432cd90b18f380c2569684665646891be55b17
gkampitakis <gkabitakis@gmail.com>
2020-06-03 19:51:13 +0100

    [client] start using dev tools on client

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,39 +1,39 @@
-import React, { useMemo } from "react";
-import configuration from "./configuration";
-import { stringify } from "query-string";
+import React, { useMemo } from 'react';
+import configuration from './configuration';
+import { stringify } from 'query-string';
 
 function App() {
-  const { twitter, fb, gh, google } = configuration;
+	const { twitter, fb, gh, google } = configuration;
 
-  const fb_params = useMemo(() => {
-    return stringify({
-      client_id: configuration.fb.client_id,
-      redirect_uri: fb.callback,
-      scope: fb.scope,
-      response_type: "code",
-      auth_type: "rerequest",
-    });
-  }, []);
+	const fb_params = useMemo(() => {
+		return stringify({
+			client_id: configuration.fb.client_id,
+			redirect_uri: fb.callback,
+			scope: fb.scope,
+			response_type: 'code',
+			auth_type: 'rerequest'
+		});
+	}, []);
 
-  const google_params = useMemo(() => {
-    return stringify({
-      client_id: configuration.google.client_id,
-      redirect_uri: google.callback,
-      scope: google.scope,
-      response_type: "code",
-      access_type: "offline",
-      prompt: "consent",
-    });
-  }, []);
+	const google_params = useMemo(() => {
+		return stringify({
+			client_id: configuration.google.client_id,
+			redirect_uri: google.callback,
+			scope: google.scope,
+			response_type: 'code',
+			access_type: 'offline',
+			prompt: 'consent'
+		});
+	}, []);
 
-  return (
-    <div className="App">
-      <a href={gh.url()}>Test Github Login</a>
-      <a href={fb.url(fb_params)}>Test Facebook Login</a>
-      <a href={twitter.url}>Test Twitter Login</a>
-      <a href={google.url(google_params)}>Test Google Login</a>
-    </div>
-  );
+	return (
+		<div className="App">
+			<a href={gh.url()}>Test Github Login</a>
+			<a href={fb.url(fb_params)}>Test Facebook Login</a>
+			<a href={twitter.url}>Test Twitter Login</a>
+			<a href={google.url(google_params)}>Test Google Login</a>
+		</div>
+	);
 }
 
 export default App;
--- client/src/configuration.ts
+++ client/src/configuration.ts
@@ -1,29 +1,26 @@
 export default {
-  api: process.env.REACT_APP_SERVER_URL,
-  gh: {
-    client_id: process.env.REACT_APP_GH_CLIENT_ID,
-    url: function () {
-      return `https://github.com/login/oauth/authorize?client_id=${this.client_id}&scope=user:read,user:email`;
-    },
-  },
-  google: {
-    client_id: process.env.REACT_APP_GOOGLE_CLIENT_ID,
-    scope: [
-      "https://www.googleapis.com/auth/userinfo.email",
-      "https://www.googleapis.com/auth/userinfo.profile",
-    ].join(" "),
-    url: (params: string) =>
-      `https://accounts.google.com/o/oauth2/v2/auth?${params}`,
-    callback: "http://localhost/api/auth/google/callback",
-  },
-  twitter: {
-    url: "http://localhost/api/auth/twitter/request-token",
-  },
-  fb: {
-    client_id: process.env.REACT_APP_FB_CLIENT_ID,
-    url: (params: string) =>
-      `https://www.facebook.com/v4.0/dialog/oauth?${params}`,
-    callback: "http://localhost/api/auth/fb/callback",
-    scope: ["email"].join(","),
-  },
+	api: process.env.REACT_APP_SERVER_URL,
+	gh: {
+		client_id: process.env.REACT_APP_GH_CLIENT_ID,
+		url: function () {
+			return `https://github.com/login/oauth/authorize?client_id=${this.client_id}&scope=user:read,user:email`;
+		}
+	},
+	google: {
+		client_id: process.env.REACT_APP_GOOGLE_CLIENT_ID,
+		scope: ['https://www.googleapis.com/auth/userinfo.email', 'https://www.googleapis.com/auth/userinfo.profile'].join(
+			' '
+		),
+		url: (params: string) => `https://accounts.google.com/o/oauth2/v2/auth?${params}`,
+		callback: 'http://localhost/api/auth/google/callback'
+	},
+	twitter: {
+		url: 'http://localhost/api/auth/twitter/request-token'
+	},
+	fb: {
+		client_id: process.env.REACT_APP_FB_CLIENT_ID,
+		url: (params: string) => `https://www.facebook.com/v4.0/dialog/oauth?${params}`,
+		callback: 'http://localhost/api/auth/fb/callback',
+		scope: ['email'].join(',')
+	}
 };
